4a43a7973ecf76ed7478673b7f5a71c0e5b91136
bryant007 <bryant.crowe@gmail.com>
2019-07-17 21:17:45 -0400

    Challenge added new string to forecast.js for displaying alert description

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -7,20 +7,22 @@ const forecast = function(latitude, longitude, callback) {
     "," +
     longitude;
 
-  request({ url: url, json: true }, function(error, response) {
+  request({ url, json: true }, function(error, body) {
     if (error) {
       callback("Unable to connect to weather service", undefined);
-    } else if (response.body.error) {
+    } else if (body.error) {
       callback("Unable to find location", undefined);
     } else {
       callback(
         undefined,
-        response.body.daily.data[0].summary +
+        body.daily.data[0].summary +
           " It is currently " +
-          response.body.currently.temperature +
+          body.currently.temperature +
           " degrees out.  There is a " +
-          response.body.currently.precipProbability +
-          " % chance of rain."
+          body.currently.precipProbability +
+          " % chance of rain, and alert follows if available: " +
+          body.alerts.description +
+          "."
       );
     }
   });
